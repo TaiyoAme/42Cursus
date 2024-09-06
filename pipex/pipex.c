9236ce8951de90481c2ce634e4e9c7aecd5df432
TaiyoAme <56363037+Taiyang999@users.noreply.github.com>
2024-09-06 19:09:07 +0200

    Add files via upload

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: hehuang <hehuang@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 16:33:00 by hehuang           #+#    #+#             */
-/*   Updated: 2024/06/24 19:01:51 by hehuang          ###   ########.fr       */
+/*   Updated: 2024/09/06 18:58:02 by hehuang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,22 +61,59 @@ char	*ft_get_env(char	*name, char	**envp)
 char	*ft_get_path(char	*cmd, char	**envp)
 {
 	char	**paths;
+	char	*exec;
 	int		i;
 
 	i = -1;
-	paths = ft_split(ft_get_env("PATH", envp), ';');
+	paths = ft_split(ft_get_env("PATH", envp), ':');
 	while (paths[++i])
 	{
-
+		paths[i] = ft_strjoin(paths[i], "/");
+		exec = ft_strjoin(paths[i], cmd);
+		if (access(exec, F_OK | X_OK) == 0)
+		{
+			free(paths);
+			return (exec);
+		}
+		free (exec);
 	}
+	free(paths);
+	return (NULL);
 }
 
-void	main_process(char	*path, int *fds)
+void	process_one(char	*path, int *fds, char *cmd, char **envp)
 {
-	int	fd;
+	int		infile;
+	char	**cmd_arg;
 
-	fd = open(path, O_RDONLY, 0444);
-	dup2(fds[0], fd);
+	cmd_arg = ft_split(cmd, ' ');
+	printf("%s ,%s\n", ft_get_path(cmd_arg[0], envp), cmd);
+	infile = open(path, O_RDONLY);
+	if (infile == -1 )
+		printf("error infile\n");
+	dup2(infile, STDIN_FILENO);
+	dup2(fds[1], STDOUT_FILENO);
+	close(fds[0]);
+	close(infile);
+	execve(ft_get_path(cmd_arg[0], envp), cmd_arg, envp);
+//free if execve fail
+}
+void	process_two(char	*path, int *fds, char *cmd, char **envp)
+{
+	int		outfile;
+	char	**cmd_arg;
+
+	cmd_arg = ft_split(cmd, ' ');
+	printf("%s ,%s\n", ft_get_path(cmd_arg[0], envp), cmd);
+	outfile = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (outfile == -1)
+		printf("error outfile\n");
+	dup2(fds[0], STDIN_FILENO);
+	dup2(outfile, STDOUT_FILENO);
+	close(outfile);
+	close(fds[1]);
+	execve(ft_get_path(cmd_arg[0], envp), cmd_arg, envp);
+//free if execve fail
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -88,22 +125,13 @@ int	main(int argc, char **argv, char **envp)
 	argv ++;
 	(void)envp;
 	if (pipe(fds) == -1)
-		printf("no\n");
-	else
-	{
-		printf("yes\n");
-	}
+		printf("pipe error\n");
 	pid = fork();
 	if (pid == -1)
-		printf("no\n");
-	else
-		printf("yes\n");
+		printf("fork error\n");
 	if (pid == 0)
-		printf("child\n");
+		process_one(argv[0], fds, argv[1], envp);
 	else
-		printf("parent\n");
-	//while (*envp)
-		//printf("%s\n",*envp++);
-	//printf("%s\n", ft_get_env("PATH", envp));
+		process_two(argv[argc - 1], fds, argv[2], envp);
 	return (0);
 }
